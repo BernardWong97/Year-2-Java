c5cab4244d3daa0d1d49714bb3df02bf3d5dcf3c
Bernard Wong <ben.wc88@gmail.com>
2017-11-09 17:15:24 +0000

    Add Abstract Class
    Create Employee abstract class with abstract and static variables and
    methods

---
+++ src/employee/Employee.java
@@ -0,0 +1,18 @@
+package employee;
+
+public abstract class Employee {
+	// Variables
+	String id, name;
+	float hourlyRate, normalNumOfHours = 37.5F;
+	static float shareValue; // static variable
+	
+	// abstract methods
+	public abstract double calculateSalary();
+	public abstract void display();
+	
+	// static methods
+	public static void setShareValue(float value){
+		shareValue = value;
+	} // setShareValue()
+
+} // class
