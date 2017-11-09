1669441199eebf5f56bd4ceaee0074617f2f8222
Bernard Wong <ben.wc88@gmail.com>
2017-11-09 18:03:26 +0000

    Add Manager class
    Create Manager class and test object

---
+++ src/employee/Manager.java
@@ -0,0 +1,54 @@
+package employee;
+
+public class Manager extends Employee {
+	// Variables
+	float managerBonus;
+	float weeklySalary, overtime;
+	
+	// Constructors
+	public Manager(String id, String name, float hourlyRate, float bonus) {
+		this.id = id;
+		this.name = name;
+		this.hourlyRate = hourlyRate;
+		setManagerBonus(bonus);
+	}
+	
+	public Manager(String id, String name, float hourlyRate, float bonus, float overtime) {
+		this(id, name, hourlyRate, bonus);
+		this.overtime = overtime;
+	}
+	
+	// Get/Set Methods
+	public void setManagerBonus(float managerBonus) {
+		this.managerBonus = managerBonus;
+	}
+	
+	public float getManagerBonus() {
+		return managerBonus;
+	}
+
+	// override calculate salary method
+	@Override
+	public double calculateSalary() {
+		//calculate weekly salary
+		weeklySalary = hourlyRate * normalNumOfHours;
+		
+		if(overtime > 0) {
+			weeklySalary += (overtime * hourlyRate * 1.5F);
+		} // if overtime, hourly rate is multiplied by 1.5
+		
+		return weeklySalary;
+	} // calculateSalary()
+
+	// override display method
+	@Override
+	public void display() {
+		System.out.println("Manager ID: " + id);
+		System.out.println("Manager name: " + name);
+		System.out.println("Manager salary for the week: " + String.format("%.2f", calculateSalary()));
+		System.out.println("Manager bonus: " + String.format("%.2f", managerBonus));
+		System.out.println("Share Value: " + String.format("%.2f", shareValue));
+		System.out.println("=======================================================================");
+	} // display()
+
+} // class
--- src/employee/TestClass.java
+++ src/employee/TestClass.java
@@ -11,7 +11,11 @@ public class TestClass {
 		HourlyWorker mary = new HourlyWorker("004", "Mary", 9.00F, 10, "Jill", 5);
 		mary.display();
 		
-
+		// create Manager object
+		Manager jill = new Manager("001", "Jill", 30.00F, 0, 10);
+		jill.setManagerBonus(1000);
+		jill.display();
+		
 	} // main
 
 } // class
